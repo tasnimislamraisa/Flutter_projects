1b296dfa4585b6936d3ab63db3ec7be9f5a4c3ff
Tasnim Islam Raisa <islamtasnim65@gmail.com>
2024-08-29 06:51:18 +0600

    expense tracker

---
+++ expense_tracker_app/lib/screens/home/views/home_screen.dart
@@ -0,0 +1,57 @@
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'main_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: AppBar(
+      //   title: const Text("Expense Tracker"),
+      // ),
+
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.white,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        //selectedItemColor: Colors.lightBlueAccent,
+        elevation: 3,
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.home),
+          label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.circle_grid_3x3_fill),
+              label: 'states')
+        ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {  },
+        shape: const CircleBorder(),
+        child:Container(
+          width: 60,
+          height: 60,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            gradient: LinearGradient(
+                colors: [
+                  Theme.of(context).colorScheme.tertiary,
+                  Theme.of(context).colorScheme.secondary,
+                  Theme.of(context).colorScheme.primary,
+                ],
+              transform:const GradientRotation(pi/4)
+
+            )
+          ),
+            child: const Icon(CupertinoIcons.add)),
+
+      ),
+      body:const MainScreen(),
+    );
+  }
+}
